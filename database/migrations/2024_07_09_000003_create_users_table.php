5abdeffa8d62546cd9264097a52095862c2b7c69
AkbarMuh <56223880+AkbarMuh@users.noreply.github.com>
2024-07-10 01:20:25 +0700

    Be (#6)
    * Create cekbranch.txt
    
    * Update cekbranch.txt
    
    * Update 2024_07_09_000003_create_users_table.php
    
    * dscsdc
    
    * Update docker-compose.yml
    
    * Squashed commit of the following:
    
    commit 0c709a839b10dd33790f5d6c18d7cefb7795c13d
    Author: Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
    Date:   Tue Jul 9 23:38:41 2024 +0700
    
        [UPDATE] Add column last update principal savings
    
    commit fb08f9863edd7786f621f0c10b9d3b4cb226b0f0
    Author: Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
    Date:   Tue Jul 9 23:23:08 2024 +0700
    
        [UPDATE] change data type
    
    commit ea007825cf24b1a60f0c11d3fae78b8ba3dfb833
    Author: Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
    Date:   Tue Jul 9 23:15:55 2024 +0700
    
        [UPDATE] Make migration for savings and transaction
    
    commit 648c5f592bf445cb90192f9d480e1c2a4b382f03
    Author: Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
    Date:   Tue Jul 9 21:37:28 2024 +0700
    
        [UPDATE] Improve performance docker
    
    commit d14b3e7d4c3d30d937d716e782e3bcf9aad788ec
    Author: Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
    Date:   Tue Jul 9 19:22:17 2024 +0700
    
        [UPDATE] Added Docker
    
    commit 9a9c0b006a245c7e79b1ac04d3c72412313674cc
    Author: Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
    Date:   Tue Jul 9 15:44:02 2024 +0700
    
        [FIX] Fixing if user already logged cannot access login page
    
    * make Transaksi

--- database/migrations/2024_07_09_000003_create_users_table.php
+++ database/migrations/2024_07_09_000003_create_users_table.php
@@ -13,6 +13,51 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('num_member')->autoIncrement();
+            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->integer('num_member')->unique();
             $table->string('name', 64);
             $table->string('username', 16)->unique();
