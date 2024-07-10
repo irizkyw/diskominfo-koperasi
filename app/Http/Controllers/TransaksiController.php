f67e616c5438cd44e872f94d30fc055246a279b3
AkbarMuh <akbar.muhammt@gmail.com>
2024-07-10 20:41:11 +0700

    Update TransaksiController.php

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -7,4 +7,26 @@ use Illuminate\Http\Request;
 class TransaksiController extends Controller
 {
     //
+    public function index()
+    {
+        return view('transaksi.index');
+    }
+
+    public function cekTransaksiAll()
+    {
+        $transaksi = Transaksi::all();
+        return response()->json($transaksi);
+    }
+
+    public function cekLogTransaksiByUserId($id)
+    {
+        $transaksi = Transaksi::where('user_id', $id)->get();
+        return response()->json($transaksi);
+    }
+
+    public function cekTransaksiSimpananByUserId($id)
+    {
+        $transaksi = Transaksi::where('user_id', $id)->where('jenis_transaksi', 'SIMPANAN-BULANAN')->get();
+        return response()->json($transaksi);
+    }
 }
