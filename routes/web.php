05be0aedd9dda23044cf68535052828d52a534ed
AkbarMuh <akbar.muhammt@gmail.com>
2024-07-10 23:26:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,42 @@ Route::middleware(['auth', AdminMiddleware::class])->group(function () {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Route::get('/admin/cekSemuaUser', [AdminContoller::class, 'cekSemuaUser']);
 
 
-Route::get('/admin/transaksi/cekTransaksiAll', [TransaksiController::class, 'cekTransaksiAll']);
+Route::get('/admin/transaksi/LogTransaksiAll', [TransaksiController::class, 'cekTransaksiAll']);
 
-Route::get('/admin/transaksi/cekTransaksiSimpananBulananByUserId/{id}', [TransaksiController::class, 'cekTransaksiSimpananBulananByUserId']);
+Route::get('/admin/transaksi/LogAllTransaksiByUserId/{id}', [TransaksiController::class, 'LogAllTransaksiByUserId']);
+Route::get('/admin/transaksi/LogTransaksiSimpananBulananByUserId/{id}', [TransaksiController::class, 'LogTransaksiSimpananBulananByUserId']);
+
 Route::get('/admin/transaksi/SumTransaksiSimpananBulananByUserId/{id}', [TransaksiController::class, 'SumTransaksiSimpananBulananByUserId']);
 Route::get('/admin/transaksi/SumTransaksiSimpananAkhirByUserId/{id}', [TransaksiController::class, 'SumTransaksiSimpananAkhirByUserId']);
 Route::get('/admin/transaksi/LaporanByUserId/{id}', [TransaksiController::class, 'LaporanByUserId']);
 
+Route::get('/admin/tabungan/cekTabunganAll', [TabunganController::class, 'cekTabunganAll']);
+Route::get('/admin/tabungan/cekTabunganByUserAuth', [TabunganController::class, 'cekTabunganByUserAuth']);
+Route::get('/admin/tabungan/cekTabunganByUser/{id}', [TabunganController::class, 'cekTabunganByUserId']);
 Route::get('/admin/tabungan/updateSimpananBulananUser/{id}', [TabunganController::class, 'updateSimpananBulananUser']);
