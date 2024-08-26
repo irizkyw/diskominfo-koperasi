f5d11e38ae866c33f11752831255b4f21c9cb95c
Ichwan Rizky Wahyudin <ichwanrizkywahyudin3@gmail.com>
2024-08-26 15:50:36 +0700

    FIX footer

--- resources/views/dashboard/pages/table_simpanan.blade.php
+++ resources/views/dashboard/pages/table_simpanan.blade.php
@@ -164,6 +164,23 @@
                 </div>
             </div>
         </div>
+        <div id="kt_app_footer" class="app-footer">
+            <!--begin::Footer container-->
+            <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
+                <!--begin::Copyright-->
+                <div class="text-gray-900 order-2 order-md-1">
+                    <span class="text-muted fw-semibold me-1">2024&copy;</span>
+                    <a href="https://github.com/IRWAPAW-Group" target="_blank"
+                        class="text-gray-800 text-hover-primary">IRWAPAW
+                        🐾</a>
+                </div>
+                <!--end::Copyright-->
+                <!--begin::Menu-->
+
+                <!--end::Menu-->
+            </div>
+            <!--end::Footer container-->
+        </div>
         <!--end::Modal - Export Simpanan-->
     </div>
 @endsection
